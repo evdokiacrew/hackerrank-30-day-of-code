04631e1fcbd71dba7c837c889731e44c8276894f
evdokiacrew <evdokiacrew@gmail.com>
2017-10-07 00:23:08 +0300

    solve day 7 challenge

---
+++ HR_30DCC_07_arrays.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func readAndReverse() {
+	//reading first string of STDIN as size of array
+	var N int
+	fmt.Scanf("%d", &N)
+	A := make([]int, N)
+
+	//readin next string of STDIN and cycle it into slice
+	for i := 0; i < N; i++ {
+		fmt.Scanf("%d", &A[i])
+	}
+
+	//reversing our slice
+	for i, j := 0, len(A)-1; i < j; i, j = i+1, j-1 {
+		A[i], A[j] = A[j], A[i]
+	}
+
+	//printing our slice
+	for i := range A {
+		fmt.Printf("%d ", A[i])
+	}
+}
+
+func main() {
+	readAndReverse()
+}
+
+//this time the code simpler and much faster without any packages (like bufio, os and strvconv)

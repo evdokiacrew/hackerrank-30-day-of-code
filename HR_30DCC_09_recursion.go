96b71a4d987bbe756aa49eefa06b966c249a98d8
evdokiacrew <evdokiacrew@gmail.com>
2017-10-09 16:19:27 +0300

    solve day 9 challenge, too easy

--- HR_30DCC_09_recursion.go
+++ HR_30DCC_09_recursion.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
+//function to compute factorial, our function get named "factorial" to get scores
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
 func main() {
 	//reading N of STDIN
 	var N int
 	fmt.Scanf("%d", &N)
+	//call our function and print result in STDOUT
+	fmt.Println(factorial(N))
 }
